6ebf465f0ec1850edd91c952f866187520a9a56b
shawnwxy0 <shawnwxy@bu.edu>
2024-12-20 11:16:51 -0500

    Update gmm.py

--- gmm.py
+++ gmm.py
@@ -23,10 +23,12 @@ class GMM():
         if colors is None:
             colors = np.random.rand(k, 3)
         self.colors = colors
+        
     def init_gibbs(self, X):
         self.data = X
         self.num_points = X.shape[0]
         self.z = np.zeros((self.num_points, self.k))
+        
     def gibbs_update(self):
         # Update cluster assignments z_i based on posterior probabilities
         for i in range(self.num_points):
@@ -44,6 +46,7 @@ class GMM():
             else:
                 # If total_posterior is zero, assign uniformly or handle appropriately
                 self.z[i] = np.random.dirichlet(np.ones(self.k))  # Uniform assignment
+                
     def sgld_update(self):
         epsilon = 1e-6  # Small constant for numerical stability
         for i in range(self.k):
@@ -54,25 +57,25 @@ class GMM():
                 gradient_mu = np.zeros(self.dim)
                 for x in assigned_points:
                     gradient_mu += multivariate_normal.pdf(x, mean=self.mu[i], cov=self.sigma[i]) * (x - self.mu[i])
-                # Fisher Information Matrix (FIM) for mu_k is simply sigma_k^{-1}
-                fim_mu_inv = np.linalg.inv(self.sigma[i])
+                # Update for mu_k using Sigma_k as a preconditioner
                 noise_mu = np.random.normal(0, np.sqrt(2 * self.learning_rate), size=self.mu[i].shape)
-                self.mu[i] += (self.learning_rate * fim_mu_inv @ gradient_mu / len(assigned_points)) + noise_mu
+                self.mu[i] += (self.learning_rate * self.sigma[i] @ gradient_mu / len(assigned_points)) + noise_mu
                 # Covariance update using SGLD with preconditioning
                 gradient_sigma = np.zeros((self.dim, self.dim))
                 for x in assigned_points:
                     diff = (x - self.mu[i]).reshape(-1, 1)
                     gradient_sigma += multivariate_normal.pdf(x, mean=self.mu[i], cov=self.sigma[i]) * (diff @ diff.T)
                 noise_sigma = np.random.normal(0, np.sqrt(2 * self.learning_rate), size=self.sigma[i].shape)
-                # FIM for sigma_k is more complex; here we assume a simple structure.
-                fim_sigma_inv = 2 * np.linalg.inv(self.sigma[i])  # Simplified assumption
-                self.sigma[i] += (self.learning_rate * fim_sigma_inv @ gradient_sigma / len(assigned_points)) + noise_sigma
+                # Update for Sigma_k using (2 Sigma_k ⊗ Sigma_k) as a preconditioner
+                preconditioned_update_sigma = (2 * self.sigma[i] @ gradient_sigma) / len(assigned_points)
+                self.sigma[i] += preconditioned_update_sigma + noise_sigma
                 # Ensure covariance is symmetric and positive definite
                 self.sigma[i] = (self.sigma[i] + self.sigma[i].T) / 2 + epsilon * np.eye(self.dim)
                 # Adjust negative eigenvalues if necessary to ensure positive definiteness
                 eigvals, eigvecs = np.linalg.eigh(self.sigma[i])
                 eigvals[eigvals < 0] = epsilon  # Set negative eigenvalues to epsilon
                 self.sigma[i] = eigvecs @ np.diag(eigvals) @ eigvecs.T
+                
     def log_likelihood(self, X):
         ll = []
         for d in X:
@@ -81,11 +84,8 @@ class GMM():
                 tot += self.pi[i] * multivariate_normal.pdf(d, mean=self.mu[i], cov=self.sigma[i])
             ll.append(np.log(tot))
         return np.sum(ll)
-
+        
     def plot_gaussian(self, mean, cov, ax, n_std=3.0, facecolor='none', **kwargs):
-        '''
-        Utility function to plot one Gaussian from mean and covariance.
-        '''
         pearson = cov[0, 1] / np.sqrt(cov[0, 0] * cov[1, 1])
         ell_radius_x = np.sqrt(1 + pearson)
         ell_radius_y = np.sqrt(1 - pearson)
@@ -104,12 +104,8 @@ class GMM():
             .translate(mean_x, mean_y)
         ellipse.set_transform(transf + ax.transData)
         return ax.add_patch(ellipse)
-
+        
     def draw(self, ax, n_std=2.0, facecolor='none', **kwargs):
-        '''
-        Function to draw the Gaussians.
-        Note: Only for two-dimensionl dataset
-        '''
         if (self.dim != 2):
             print("Drawing available only for 2D case.")
             return
